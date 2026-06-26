40a4b4ada9ddc5ec55043456991ea692499ea53f
miruto2625161 <oom2625161@gmail.com>
2026-06-26 12:18:26 +0900

    お問い合わせ更新画像サイズ変更

---
+++ Contact.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    if(name === "" || email === "" || message === ""){
+        document.getElementById("result").textContent =
+        "入力してください。";
+        return;
+    }
+    
+});
